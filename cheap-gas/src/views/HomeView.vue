44db485c5759523cb5291967d222c69d1e7f7957
Ricardo Castro <ricardocastrodesign@gmail.com>
2023-10-28 12:05:59 +0100

    test gasStations

--- cheap-gas/src/views/HomeView.vue
+++ cheap-gas/src/views/HomeView.vue
@@ -1,7 +1,37 @@
 <template>
-  <HelloWorld />
+  <ul v-for="station in gasStations" :key="station.id">
+    <li>{{ station.name }} - {{ station.price }}</li>
+  </ul>
 </template>
 
-<script setup>
-  import HelloWorld from '@/components/HelloWorld.vue'
+<script>
+import { getGasStations } from "@/api/apiService.js"; // Import the API service
+
+export default {
+  components: {},
+  data() {
+    return {
+      gasStations: [],
+    };
+  },
+  mounted() {
+    this.getGasStations();
+  },
+  methods: {
+    async getGasStations() {
+      const searchForm = {
+        lat: 52.521,
+        lng: 13.438,
+        rad: 15,
+        type: "diesel",
+        sort: "dist",
+      };
+      try {
+        this.gasStations = await getGasStations(searchForm);
+      } catch (error) {
+        console.error("Error fetching gas stations:", error);
+      }
+    },
+  },
+};
 </script>
