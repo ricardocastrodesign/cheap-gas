c5ba147d5e2b1a495cfd883aa4a229216db5dd0d
Ricardo Castro <ricardocastrodesign@gmail.com>
2023-10-28 11:59:14 +0100

    add api request to get gas stations

---
+++ cheap-gas/src/api/apiService.js
@@ -0,0 +1,29 @@
+import axios from 'axios';
+
+const API_KEY = 'd28cef5b-8bd3-1d58-236f-1ed31dcf0697';
+
+const baseURL = 'https://creativecommons.tankerkoenig.de/json';
+
+// Function to fetch gas stations based on search criteria
+export async function getGasStations(searchParams) {
+  try {
+    const response = await axios.get(`${baseURL}/list.php`, {
+      params: {
+        lat: searchParams.lat,
+        lng: searchParams.lng,
+        rad: searchParams.rad,
+        sort: searchParams.sort,
+        type: searchParams.type,
+        apikey: API_KEY,
+      },
+    });
+
+    if (response.data.ok) {
+      return response.data.stations;
+    } else {
+      throw new Error(response.data.message);
+    }
+  } catch (error) {
+    throw new Error('An error occurred while fetching gas stations: ' + error);
+  }
+}
